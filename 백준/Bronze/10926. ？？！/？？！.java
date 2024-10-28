e88a6b950cbee4951b2e6a59635cb4ff4d113372
niceCityGocheok <dmsvy9259@naver.com>
2024-10-29 01:25:21 +0900

    [Bronze V] Title: ??!, Time: 180 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        String id = in.nextLine();
+        System.out.println(id + "??!");
+
+
+
+    }
+}
