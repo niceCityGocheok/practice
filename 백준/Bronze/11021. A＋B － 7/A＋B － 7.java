d01f688f4280fc2b2828b0361515390e4719efac
niceCityGocheok <dmsvy9259@naver.com>
2024-10-30 22:18:26 +0900

    [Bronze V] Title: A+B - 7, Time: 216 ms, Memory: 18664 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int arr[] = new int[n];
+
+
+        for (int i = 0; i < n; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            arr[i] = a + b;
+
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Case #" + (i + 1) + ": " + arr[i]);
+
+        }
+
+
+
+
+        }
+
+
+}
+
+
+
+
