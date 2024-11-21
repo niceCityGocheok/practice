94f585554fb8dafa54f0707a488a2b72f567ab82
niceCityGocheok <dmsvy9259@naver.com>
2024-11-21 18:57:09 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        String str = String.valueOf(x);
+        for(int i = 0; i < str.length(); i++){
+            sum += str.charAt(i) - '0';
+        }
+        if(x % sum == 0){
+            return true;
+        }
+        return false;
+    }
+}
