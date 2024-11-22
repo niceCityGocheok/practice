63ef6ce9d7d7c58f74481d09b06d3c0826368ff2
niceCityGocheok <dmsvy9259@naver.com>
2024-11-22 09:52:14 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -3,9 +3,8 @@ class Solution {
         double answer = 0;
         for(int i = 0; i < arr.length; i++){
             answer += arr[i];
-            
         }
-        answer/=arr.length;
-        return answer;
+        
+        return answer/arr.length;
     }
 }
